65eec23776d3f234291f23122da10909458118b3
vcokltfre <vcokltfre@gmail.com>
2026-04-28 14:49:51 +0100

    feat: implement PWA

--- src/dashboard/vite.config.ts
+++ src/dashboard/vite.config.ts
@@ -2,9 +2,37 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  plugins: [
+    devtools(),
+    solidPlugin(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Packlog",
+        short_name: "Packlog",
+        theme_color: "#87CEEB",
+        background_color: "#1E1E1E",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
